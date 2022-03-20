c7b0a14018300acc2d37fa1a9e0cf773fa7dfcfb
Vazgen <vazgen04@gmail.com>
2022-03-20 14:13:48 +0400

    feature/repository

---
+++ src/main/java/repository/PassportFileRepository.java
@@ -0,0 +1,7 @@
+package repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PassportFileRepository extends JpaRepository<PassportFile, Integer> {
+
+}
---
+++ src/main/java/repository/SocialCardRepository.java
@@ -0,0 +1,7 @@
+package repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SocialCardRepository extends JpaRepository<SocialCard, Integer> {
+
+}
